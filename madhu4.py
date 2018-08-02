748ad7df73f3c6a4af749e7ce17c8644b4fe4854
MadhaviLavu <41770333+MadhaviLavu@users.noreply.github.com>
2018-08-02 15:03:49 +0530

    Update and rename madhu3.py to madhu4.py

---
+++ madhu4.py
@@ -0,0 +1,10 @@
+val=input()
+if val>='a' and val<='z' or val>='A' and val<='Z':
+	if val=='a' or val=='e' or val=='i' or val=='o' or val=='u' or val=='A' or val=='E' or val=='I' or val=='O' or val=='U':
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
+	
+	
